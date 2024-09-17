8cfc6654689b39abde4b894d671844222b78ac5b
Maxim <maxkryu2004@gmail.com>
2024-09-17 21:49:03 +0300

    socket url and title updated

--- backend/socket/soket.js
+++ backend/socket/soket.js
@@ -8,7 +8,7 @@ import express from "express";
 
  const io = new  Server(server,{
     cors:{
-        origin:["http://localhost:5173"],
+        origin:["https://lizchat.onrender.com"],
         methods:["GET", "POST"]
     }
 })
