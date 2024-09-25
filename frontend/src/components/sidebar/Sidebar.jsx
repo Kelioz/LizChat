0952dfd1531088871a04aff6a4333d1583831347
Maxim <maxkryu2004@gmail.com>
2024-09-25 17:59:30 +0300

    adaptive update

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -2,18 +2,27 @@ import SearchInput from "../../ui/Searchinput/SearchInput.jsx";
 import Conversations from "./Conversations.jsx";
 import LogoutButton from "./LogoutButton.jsx";
 import useConversation from "../../zustand/useConversation.js";
+import {GoArrowLeft} from "react-icons/go";
+
 
 
 export default function Sidebar(){
-    const {selectedConversation} = useConversation()
+    const {selectedConversation,setSelectedConversation} = useConversation()
     return(
         <>
-                <div className={"border-r border-slate-500 flex flex-col md:min-w-[150px] lg:min-w-[350px]"}>
-                    <SearchInput />
-                    <div className={"divider px-3"}></div>
-                    <Conversations />
-                    <LogoutButton />
-                </div>
+        {selectedConversation ?
+            <div className=" flex flex-col md:min-w-[150px] lg:min-w-[350px]">
+                <GoArrowLeft onClick={setSelectedConversation(null)} />
+                <LogoutButton />
+            </div>
+            :
+            <div className={"border-r border-slate-500 flex flex-col md:min-w-[150px] lg:min-w-[350px]"}>
+                <SearchInput />
+                <div className={"divider px-3"}></div>
+                <Conversations />
+                <LogoutButton />
+            </div>
+                }
 
 
         </>
