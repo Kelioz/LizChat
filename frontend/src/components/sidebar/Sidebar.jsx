8a9b7bf21b8abeb9724ff82144b244af6b052c3b
Maxim <maxkryu2004@gmail.com>
2024-09-26 21:32:32 +0300

    fix

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -6,9 +6,9 @@ import useConversation from "../../zustand/useConversation.js";
 
 
 export default function Sidebar(){
-    const {selectedConversation} = useConversation()
+
     return(
-        <>
+
                 <div className={"border-r border-slate-500 flex flex-col md:min-w-[150px] lg:min-w-[350px]"}>
                     <SearchInput />
                     <div className={"divider px-3"}></div>
@@ -16,6 +16,5 @@ export default function Sidebar(){
                     <LogoutButton />
                 </div>
 
-        </>
     )
 }
